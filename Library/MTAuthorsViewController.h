81d436db257045eab3c53a58bb10f92f98a0d319
gongli2000 <larrye2000@gmail.com>
2014-02-12 20:28:44 -0800

    new

---
+++ Library/MTAuthorsViewController.h
@@ -0,0 +1,15 @@
+//
+//  MTAuthorsViewController.h
+//  Library
+//
+//  Created by Bart Jacobs on 19/12/12.
+//  Copyright (c) 2012 Mobile Tuts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTAuthorsViewController : UITableViewController
+
+@property NSArray *authors;
+
+@end
---
+++ Library/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Library
+//
+//  Created by Bart Jacobs on 19/12/12.
+//  Copyright (c) 2012 Mobile Tuts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTAppDelegate class]));
+    }
+}
